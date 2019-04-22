0c5795276c9627f9e27ff4a27a846087dda9ee34
Asia9847 <43933595+Asia9847@users.noreply.github.com>
2019-04-22 22:38:20 +0800

    Update settings.py
    修正配置文件

--- appbackend/settings.py
+++ appbackend/settings.py
@@ -150,4 +150,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR=[BASE_DIR,'dist/static']
+STATICFILES_DIRS=[BASE_DIR,'dist/static']
